cee252a1f87fafe882eff4db5ba0db583b240abc
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-09-29 17:51:38 +0300

    corrected

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -29,14 +29,17 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this.fullName} - ${this.holbertonClass.year} - ${this.holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-export const listOfStudents = [
-  new StudentHolberton('Guillaume', 'Salva', new HolbertonClass(2020, 'San Francisco')),
-  new StudentHolberton('John', 'Doe', new HolbertonClass(2020, 'San Francisco')),
-  new StudentHolberton('Albert', 'Clinton', new HolbertonClass(2019, 'San Francisco')),
-  new StudentHolberton('Donald', 'Bush', new HolbertonClass(2019, 'San Francisco')),
-  new StudentHolberton('Jason', 'Sandler', new HolbertonClass(2019, 'San Francisco'))
-];
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+export default [student1, student2, student3, student4, student5];

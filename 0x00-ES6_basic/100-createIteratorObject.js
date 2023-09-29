b707c0aa3c9b516e3af0b61491a6bf082a5f65bc
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-09-29 18:35:34 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,30 +1,8 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-  let departmentIndex = 0;
-  let employeeIndex = 0;
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
 
-  return {
-    [Symbol.iterator]() {
-      return this;
-    },
-    next() {
-      if (departmentIndex < departments.length) {
-        const department = departments[departmentIndex];
-        if (employeeIndex < department.length) {
-          return {
-            value: department[employeeIndex++],
-            done: false,
-          };
-        } else {
-          departmentIndex++;
-          employeeIndex = 0;
-          return this.next();
-        }
-      } else {
-        return {
-          done: true,
-        };
-      }
-    },
-  };
+  return myarray;
 }

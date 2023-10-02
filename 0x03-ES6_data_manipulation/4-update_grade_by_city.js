48b3c21f409903aaf8ceddaf38d54c2f47c8fce1
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-02 19:11:53 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const matchingGrade = newGrades.find(gradeObj => gradeObj.studentId === student.id);
+    .filter((student) => student.location === city)
+    .map(student) => {
+      const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
       return {
         ...student,
         grade: matchingGrade ? matchingGrade.grade : 'N/A'

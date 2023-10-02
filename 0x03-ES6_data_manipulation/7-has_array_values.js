6b927bffa42bd43fc7c195aaa6c55caec8e46069
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-10-02 19:24:22 +0300

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,10 @@
-function hasValuesFromArray(set, array) {
-  return array.every(value => set.has(value));
-}
+export default function hasValuesFromArray(set, list) {
+  let verdict = true;
+  list.map((x) => {
+    if (!set.has(x)) {
+      verdict = false;
+    }
+  });
 
-export default hasValuesFromArray;
+  return verdict;
+}
